2c94ddadefa7a713aa9474a622afe0e5679aeccb
PankajVeerma <pankajverma3242@gmail.com>
2023-12-28 22:08:52 +0530

    Insertion Sort

---
+++ InsertionSort.py
@@ -0,0 +1,12 @@
+def insertion_sort(list1):
+    for i in  range(1,len(list1)):
+        temp = list1[i]
+        j = i-1
+        while j>=0 and temp<list1[j]:
+            list1[j+1] =list1[j]
+            j-=1
+        list1[j+1] = temp
+            
+l = [32,54,67,39,56,87,454,34]        
+insertion_sort(l)
+print(l)           
